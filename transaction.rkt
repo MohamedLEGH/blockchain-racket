13d4124f65638382694c0cff8df8a71f43889952
Mohamed Amine LEGHERABA <mlegheraba@protonmail.com>
2023-05-08 03:46:49 +0200

    cleaning

--- transaction.rkt
+++ transaction.rkt
@@ -2,18 +2,18 @@
 ; need to add verif tx
 (struct tx (sender receiver value) #:prefab)
 
-(define (tx->jsexpr tx_val)
+(define (tx->jsexpr tx-val)
   (hash 'sender
-        (tx-sender tx_val)
+        (tx-sender tx-val)
         'receiver
-        (tx-receiver tx_val)
+        (tx-receiver tx-val)
         'value
-        (tx-value tx_val)))
+        (tx-value tx-val)))
 
-(define (tx->string tx_val)
-  (string-append (tx-sender tx_val)
-                 (tx-receiver tx_val)
-                 (number->string (tx-value tx_val))))
+(define (tx->string tx-val)
+  (string-append (tx-sender tx-val)
+                 (tx-receiver tx-val)
+                 (number->string (tx-value tx-val))))
 
 (provide (struct-out tx)
          tx->string
--- utils.rkt
+++ utils.rkt
@@ -2,9 +2,9 @@
 (require racket/string)
 (require racket/system)
 
-(define (pretty-print-json json_str)
+(define (pretty-print-json json-str)
   ; for escaping '\' and '"' characters
-  (define str_clean (string-replace json_str "\"" "\\\""))
-  (system (string-append "echo \"" str_clean "\" | jq .")))
+  (define str-clean (string-replace json-str "\"" "\\\""))
+  (system (string-append "echo \"" str-clean "\" | jq .")))
 
 (provide pretty-print-json)
